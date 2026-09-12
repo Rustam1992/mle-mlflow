93c46958b8e88237ce022ccdcd7553c84cedf40e
Rustam Mutalipov <mutalipov.rustam@gmail.com>
2026-09-12 19:18:12 +0000

    было проведено обучение на обработанных данных, значительной разницы не получил в сравнении с классическим методом

--- run_mlflow_server.sh
+++ run_mlflow_server.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+set -a
+source "$(dirname "$0")/.env"
+set +a
 
 export MLFLOW_S3_ENDPOINT_URL=https://storage.yandexcloud.net
-export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-export AWS_BUCKET_NAME=$S3_BUCKET_NAME
+export AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"
+export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
+export AWS_BUCKET_NAME="$S3_BUCKET_NAME"
 
 mlflow server \
   --backend-store-uri "postgresql://${DB_DESTINATION_USER}:${DB_DESTINATION_PASSWORD}@${DB_DESTINATION_HOST}:${DB_DESTINATION_PORT}/${DB_DESTINATION_NAME}" \
